e6827e4ee26b4faf1becfe7d2819b03402d3e61c
Adetayo Omotomiwa <adetayoomotomiwa99@gmail.com>
2021-06-10 17:12:36 +0100

    Working With API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
-import React from 'react'
-import "./App.css";
+import React, {useState, useEffect} from 'react'
+
 function App() {
+  const [vaL, setVal] = useState([]);
+  const [input, setInput] = useState('posts');
+  const [loading, setloading] = useState(true);
+
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${input}`)
+    .then(res => res.json())
+    .then(data => {
+
+      console.log(data)
+      setVal(data);
+      setTimeout(() => {
+        setloading(false)
+      }, 5000);
+    })
+    
+  }, [input])
+
+  const deletePost = (id) => {
+    const removeItem = vaL.filter(x => x.id !== id)
+
+    setVal(removeItem);
+  }
+
   return (
-    <div className="grid-container">
-        <header>
-          <a href="/">React Shopping Cart</a>
-        </header>
-        <main>
-          Product List
-        </main>
-        <footer>
-          All right is reserved
-        </footer>
-    </div>
+    <>
+      {loading ? 
+      <div className="spinner-display">
+        <div className="spinner-grow text-primary" role="status">
+  <span className="visually-hidden">Loading...</span>
+</div>
+<div className="spinner-grow text-secondary" role="status">
+  <span className="visually-hidden">Loading...</span>
+</div>
+<div className="spinner-grow text-success" role="status">
+  <span className="visually-hidden">Loading...</span>
+</div>
+<div className="spinner-grow text-danger" role="status">
+  <span className="visually-hidden">Loading...</span>
+</div>
+      </div>
+     : 
+      <>
+         <h1> Number of Posts: {vaL.length}</h1>
+        {vaL.map(x => {
+            return (
+              <div onClick={() => deletePost(x.id)}>
+              <h1>{x.id}</h1>
+              <p>{x.title}</p>
+            </div>
+            )
+        })}
+      </>
+    }</>
   )
 }
 
 export default App
+
+  
